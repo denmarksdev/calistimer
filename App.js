b403985a2a1c237de620bb8a9c528cce8c87eb97
Denis Marques <denmarksdev@gmail.com>
2019-04-27 17:57:38 -0300

    Ädicionando navegação

---
+++ App.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import { Platform, StyleSheet, Text, View } from 'react-native';
+import HomeScreen from './src/screens/HomeScreen';
+import EMOMScreen from './src/screens/EMOMScree';
+
+import {
+  createStackNavigator,
+  createAppContainer
+} from 'react-navigation'
+
+const AppNavigator = createStackNavigator({
+  Home: HomeScreen,
+  EMOM: EMOMScreen
+}, { initialRouteName: 'Home' })
+
+export default createAppContainer(AppNavigator)
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 48,
+    textAlign: 'center',
+    margin: 10,
+    fontFamily: 'Ubuntu-Bold'
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import {
+    View,
+    Text,
+    TouchableHighlight
+} from 'react-native'
+
+const HomeScreen = props => {
+    return (
+        <View>
+            <TouchableHighlight onPress={() => props.navigation.navigate('EMOM') }>
+                <Text>Home Screen</Text>
+            </TouchableHighlight>
+        </View>
+    )
+}
+
+export default HomeScreen
